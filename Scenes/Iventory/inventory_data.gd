56971f65cc067617eaadd5b0287290712efed9e0
JBEATT <jbeatt202@caledonian.ac.uk>
2023-07-11 14:17:20 +0100

    Added initial files, player controller working in basic scene, working on inventory system

---
+++ Scenes/Iventory/inventory_data.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name IventoryData
+
+@export var slot_datas: Array[SlotData]
---
+++ Scenes/Player/player.gd
@@ -0,0 +1,61 @@
+extends CharacterBody3D
+
+
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.5
+const JUMP_SPEED = 8     #(HOW FAST YOU MOVE IN AIR)
+const JUMP_VELOCITY = 3.5 #(JUMP HEIGHT ESSENTIALLY)
+const SENSITIVITY = 0.003
+
+var speed
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-75), deg_to_rad(75))
+		
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+	
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Handle Sprint.
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "foward", "back")
+	var direction = (head.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 10.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 10.0)
+	else:
+			velocity.x = direction.x * JUMP_SPEED
+			velocity.z = direction.z * JUMP_SPEED
+			
+	move_and_slide()
